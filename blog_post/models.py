0b4029ebe8fca7ba156c05a4258c3ab64a2070df
Prakash5209 <prakashkchaudhary5209@gmail.com>
2023-09-09 16:35:12 +0545

    blog image updated

--- blog_post/models.py
+++ blog_post/models.py
@@ -8,4 +8,8 @@ class Faq_model(models.Model):
 		return self.title
 
 class Blog_photo(models.Model):
+	title = models.CharField(max_length = 255)
 	image = models.ImageField(upload_to="Blog",blank=True,null=True)
+
+	def __str__(self):
+		return self.title
--- blog_post/views.py
+++ blog_post/views.py
@@ -5,5 +5,5 @@ from blog_post.models import Faq_model,Blog_photo
 def blog(request):
 	modls = Faq_model.objects.all()
 	blog_photo = Blog_photo.objects.all()
-	context = {'modls':modls,'Blog_photo':Blog_photo}
+	context = {'modls':modls,'blog_photo':blog_photo}
 	return render(request,'blog.html',context)
